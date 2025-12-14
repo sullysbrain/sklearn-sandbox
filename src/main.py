50f869cc23ac6586b66bd41b34a48ce797d3bf63
Scott Sullivan <sullivan@digitalblacksmiths.com>
2025-12-14 00:36:58 +0000

    Added polars to functionality

--- src/main.py
+++ src/main.py
@@ -1,27 +1,15 @@
+import os
 import polars as pl
 import pandas as pd
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 
 
 def main():
 
-    # Load the Iris dataset
-    iris = load_iris()
+    s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
 
-    # Create a logistic regression model
-    model = LogisticRegression(max_iter=20)
-
-    # Train the model using the training set
-    model.fit(X_train, y_train)
-
-    # Evaluate the model using the testing set
-    accuracy = model.score(X_test, y_test)
-    print(f"Accuracy: {accuracy:.2f}")
+    # df = pd.read_csv(s, header=None, encoding='utf-8')
+    # df = pl.read_csv
 
 
 
