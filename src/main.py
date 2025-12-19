6a4b3cc033bcd5a729b326da327a51a3203e8772
Scott Sullivan <scott@sullysbrain.com>
2025-12-19 04:20:07 +0000

    Plotting SGD and loss over epoch with Matplotlib and stylesheet

--- src/main.py
+++ src/main.py
@@ -11,6 +11,42 @@ from sklearn.linear_model import Perceptron
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 
+from adaline_sgd import AdalineSGD
+from matplotlib.colors import ListedColormap
+
+
+# ------------------------------------
+# Function to plot decision regions
+# ------------------------------------
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=f'Class {cl}',
+                    edgecolor='black')
+
+
+
 def main():
 
     s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -27,22 +63,36 @@ def main():
     plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='Setosa')
     plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='s', label='Versicolor')
 
-    # plt.xlabel('Sepal length [cm]')
-    # plt.ylabel('Petal length [cm]')
-    # plt.legend(loc='upper left')
-    # # plt.show()
-    # plt.savefig("results/plot.png", dpi=150, bbox_inches="tight")
+    a = AdalineSGD()
+    a.test()
 
-    ppn = Perceptron(eta0=0.1, max_iter=10)
-    ppn.fit(X, y)
-    plt.plot(range(1, len(ppn.errors_) + 1),
-    ppn.errors_, marker='o')
-    plt.xlabel('Epochs')
-    plt.ylabel('Number of updates')
-    # plt.show()
-    plt.savefig("results/plot2.png", dpi=150, bbox_inches="tight")
+    X_std = np.copy(X)
+    X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+    X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
 
+    ada_sgd = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+    ada_sgd.fit(X_std, y)
 
+    plt.style.use('seaborn-v0_8')
+    fig1, ax1 = plt.subplots()
+    plot_decision_regions(X_std, y, classifier=ada_sgd)
+    ax1.set_title('Adaline - Stochastic gradient descent')
+    ax1.set_xlabel('Sepal length [standardized]')
+    ax1.set_ylabel('Petal length [standardized]')
+    ax1.legend(loc='upper left')
+    fig1.tight_layout()
+    fig1.savefig(os.path.join('results', 'adaline_sgd_iris1.png'))
+    plt.close(fig1)
+
+    plt.style.use('seaborn-v0_8')
+    fig2, ax2 = plt.subplots()
+    ax2.plot(range(1, len(ada_sgd.losses_) + 1), ada_sgd.losses_,
+             marker='o')
+    ax2.set_xlabel('Epochs')
+    ax2.set_ylabel('Average loss')
+    fig2.tight_layout()
+    fig2.savefig(os.path.join('results', 'adaline_sgd_iris2.png'))
+    plt.close(fig2)
 
 
 if __name__ == "__main__":
